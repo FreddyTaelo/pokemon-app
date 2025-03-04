a0b295cd2110c69bfa3271e374a1c1a7e9142cbe
Freddy_Maepa <freddy.maepa@adaptit.com>
2025-03-04 23:32:30 +0200

    update base url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    API_BASE: 'http://10.245.208.78:5000/api', 
+    API_BASE: 'http://ec2-13-60-74-36.eu-north-1.compute.amazonaws.com:5000/api', 
   };
 
 export default config;
